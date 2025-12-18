86ca9729feab95d8e86f06e129d4e756a6f11262
Anushka020210 <shankaranushka06@gmail.com>
2025-12-18 13:57:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,46 +1,36 @@
 import os
-import urllib.parse
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 
 class Config(object):
-    # -------------------------
+
     # Flask
-    # -------------------------
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 
-    # -------------------------
-    # Azure Blob Storage
-    # -------------------------
-    BLOB_ACCOUNT = os.environ.get("BLOB_ACCOUNT")
-    BLOB_STORAGE_KEY = os.environ.get("BLOB_STORAGE_KEY")
-    BLOB_CONTAINER = os.environ.get("BLOB_CONTAINER")
-
-    # -------------------------
-    # Azure SQL Database
-    # -------------------------
+    # Azure SQL
     SQL_SERVER = os.environ.get("SQL_SERVER")
     SQL_DATABASE = os.environ.get("SQL_DATABASE")
     SQL_USER_NAME = os.environ.get("SQL_USER_NAME")
     SQL_PASSWORD = os.environ.get("SQL_PASSWORD")
 
-    # ---- CORRECT Azure SQL connection string ----
-    params = urllib.parse.quote_plus(
-        "DRIVER={ODBC Driver 17 for SQL Server};"
-        f"SERVER={SQL_SERVER};"
-        f"DATABASE={SQL_DATABASE};"
-        f"UID={SQL_USER_NAME};"
-        f"PWD={SQL_PASSWORD};"
+    SQLALCHEMY_DATABASE_URI = (
+        "mssql+pyodbc://"
+        + SQL_USER_NAME
+        + ":"
+        + SQL_PASSWORD
+        + "@"
+        + SQL_SERVER
+        + ":1433/"
+        + SQL_DATABASE
+        + "?driver=ODBC+Driver+17+for+SQL+Server"
     )
-
-    SQLALCHEMY_DATABASE_URI = f"mssql+pyodbc:///?odbc_connect={params}"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # -------------------------
-    # Microsoft Entra ID (OAuth)
-    # -------------------------
+    # Azure Blob
+    BLOB_ACCOUNT = os.environ.get("BLOB_ACCOUNT")
+    BLOB_STORAGE_KEY = os.environ.get("BLOB_STORAGE_KEY")
+    BLOB_CONTAINER = os.environ.get("BLOB_CONTAINER")
+
+    # OAuth
     CLIENT_ID = os.environ.get("CLIENT_ID")
     CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 
@@ -48,7 +38,4 @@ class Config(object):
     REDIRECT_PATH = "/getAToken"
     SCOPE = ["User.Read"]
 
-    # -------------------------
-    # Session
-    # -------------------------
     SESSION_TYPE = "filesystem"
